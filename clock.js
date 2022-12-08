c658a74108c9a6fc4c81ec314006bfe7d18e2c6d
Gaurav Thapa <gaurav16547@gmail.com>
2022-12-08 21:35:43 +0545

    both format works partially

--- clock.js
+++ clock.js
@@ -1,12 +1,35 @@
-
-
 let clockApp = () => {
     const hoursDisplay = document.querySelector("#hours");
     const minutesDisplay = document.querySelector("#minutes");
     const secondsDisplay = document.querySelector("#seconds");
     const timeDisplay = document.querySelector(".time");
+    const twelveHourFormat = document.querySelector("#twelve-hour");
+    const twentyfourHourFormat = document.querySelector("#twentyfour-hour");
 
-    function currentTime() {
+    function twelveHourFormatTime() {
+        const time = new Date();
+        const hours = time.getHours();
+        const minutes = time.getMinutes();
+        const seconds = time.getSeconds();
+        if (hours > 12) {
+            hoursDisplay.textContent = hours - 12;
+        }
+        else {
+            hoursDisplay.textContent = hours;
+        }
+        minutesDisplay.textContent = minutes;
+        secondsDisplay.textContent = seconds;
+
+    }
+    twelveHourFormat.addEventListener('click', () => {
+        setInterval(twelveHourFormatTime, 1000);
+        twelveHourFormat.style.display = 'none';
+        twentyfourHourFormat.style.display = 'flex';
+    })
+
+
+
+    function twentyFourHourFormatTime() {
         const time = new Date();
         const hours = time.getHours();
         const minutes = time.getMinutes();
@@ -16,6 +39,11 @@ let clockApp = () => {
         secondsDisplay.textContent = seconds;
 
     }
-    setInterval(currentTime, 1000);
+    twentyfourHourFormat.addEventListener('click', () => {
+        setInterval(twentyFourHourFormatTime, 1000);
+        twentyfourHourFormat.style.display = 'none';
+        twelveHourFormat.style.display = 'flex';
+    });
+
 }
 clockApp();
